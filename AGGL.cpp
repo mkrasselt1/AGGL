01ed8919e12f4b10bcb9727d372452ff1edb80dd
jul10104 <julius.schneider@ipms.fraunhofer.de>
2023-01-09 10:59:12 +0100

    ADD: Basic Concept

---
+++ AGGL.cpp
@@ -0,0 +1,117 @@
+#include "AGGL.h"
+#include <vector>
+
+namespace AGGL
+{
+
+    std::vector<displayInterface*> displays;
+    std::vector<graphicsHandle*> elements;
+    COLOR_MODE::colormode colormode;
+    int32_t background = COLORS::BLACK;
+
+
+
+
+    displayInterface::displayInterface(uint16_t width, uint16_t height)
+    {
+        _size.x = 0;
+        _size.y = 0;
+        _size.w = width;
+        _size.h = height;
+    }
+
+    box* displayInterface::getSize()
+    {
+        return _size;
+    }
+
+    graphicsHandle::graphicsHandle()
+    {
+        elements.push_back(this);
+    }
+
+    graphicsHandle::removeHandle()
+    {
+        if(std::find(elements.begin(), elements.end(), this) != elements.end()){
+            elements.erase(std::find(elements.begin(), elements.end(), this));
+        }
+    }
+
+    void graphicsHandle::getUpdateArea(box * oldArea, box * newArea)
+    {
+        *oldArea = _oldArea;
+        *newArea = _newArea;
+        _newArea = _oldArea;
+        _needUpdate = false;
+    }
+
+    bool graphicsHandle::needUpdate()
+    {
+        return _needUpdate;
+    }
+
+    void graphicsHandle::show()
+    {
+        _visible = true;
+        _needUpdate = true;
+    }
+
+    void graphicsHandle::hide()
+    {
+        _visible = false;
+        _needUpdate = true;
+    }
+
+
+    textHandle::textHandle(int16_t x, int16_t y, const char* text, const uint8_t * font);
+    int32_t textHandle::getPixelAt(int16_t x, int16_t y);
+
+    STATUS::code addDisplay(displayInterface* display)
+    {
+        displays.push_back(display);
+    }
+
+    STATUS::code update()
+    {
+        //Walk through elements Background to Foreground
+        for (auto const& elem:elements)
+        {
+            if(elem->needUpdate())
+            {
+                box bbOld;
+                box bbNew;
+                elem->getUpdateArea(bbOld, bbNew);
+
+                //step1: Redraw old bounding box
+                uint8_t *buf = new uint8_t[bbOld.w*bbOld.h];
+
+            }
+        }
+    }
+
+    int32_t getPixelAt(int16_t x, int16_t y)
+    {
+        //Walk through elements Background to Foreground and compute pixel
+        int32_t cPixel = COLORS::TRANSPARENT;
+        for (auto const& elem:elements)
+        {
+            if(elem->needUpdate())
+            {
+                int32_t temp = elem->getPixelAt(x, y);
+                if(temp != COLORS::TRANSPARENT)
+                {
+                    cPixel = temp;
+                }
+            }
+        }
+    }
+
+    STATUS::code setColorMode(COLOR_MODE::colormode mode)
+    {
+        colormode = mode;
+    }
+    
+} // namespace AGGL
+
+
+
---
+++ AGGL.h
@@ -0,0 +1,102 @@
+#ifndef AGGL_H
+#define AGGL_H
+#include <Arduino.h>
+
+namespace AGGL
+{
+    namespace STATUS
+    {
+        enum code{
+            OK,
+            GENERAL_ERROR,
+            OUT_OF_MEMORY,
+            FONT_NOT_SUPPORTED
+        }; 
+    } // namespace STATUS
+
+    namespace COLOR_MODE
+    {
+        enum colormode{
+            TWOCOLOR,
+            RGB_8BIT,
+            RGB_16BIT,
+            RGB_24BIT
+        }; 
+    } // namespace COLOR_MODE
+
+    namespace COLORS
+    {
+        enum color{
+            TRANSPARENT = -1,
+            BLACK =     0x00000000,
+            WHITE =     0x00FFFFFF,
+            RED =       0x000000FF,
+            GREEN =     0x0000FF00,
+            BLUE =      0x00FF0000,
+        }; 
+        int32_t fromRGB(uint8_t R, uint8_t G, uint8_t B);
+    } // namespace COLORS
+
+    typedef struct{
+        int16_t x;
+        int16_t y;
+        uint16_t w;
+        uint16_t h;
+    } box;
+    
+
+    class displayInterface
+    {
+        protected:
+        box _size = {0};
+        public:
+        displayInterface(uint16_t width, uint16_t height);
+        virtual ~displayInterface(){}
+        virtual STATUS::code init(){return STATUS::GENERAL_ERROR;}
+        virtual STATUS::code update(box bb, uint8_t* buffer){return STATUS::GENERAL_ERROR;}
+        box* getSize();
+    };
+
+    class graphicsHandle
+    {
+        private:
+        void getUpdateArea(box * oldArea, box * newArea);
+        bool needUpdate();
+        void removeHandle();
+
+        protected:
+        bool _visible = false;
+        bool _needUpdate = false;
+        box _oldArea;
+        box _newArea;
+        int32_t _foreground;
+        int32_t _background;
+
+        public:
+        graphicsHandle();
+        virtual ~graphicsHandle(){removeHandle();}
+        virtual int32_t getPixelAt(int16_t x, int16_t y){return 0;}
+        void show();
+        void hide();
+
+        friend STATUS::code update();
+    };
+
+    class textHandle : public graphicsHandle
+    {
+        private:
+        const char * _text = nullptr;
+        const uint8_t * _font = nullptr;
+
+        public:
+        textHandle(int16_t x, int16_t y, const char* text, const uint8_t * font);
+        int32_t getPixelAt(int16_t x, int16_t y);      
+    };
+
+    STATUS::code addDisplay(displayInterface* display);
+    STATUS::code update();
+    STATUS::code setColorMode(COLOR_MODE::colormode mode);
+} // namespace AGGL
+
+
+#endif
